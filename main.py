a1c5c7fc8604248c15f6847e66452365ca56e5db
christin <christin.engstrom@gmail.com>
2015-12-15 12:52:05 -0800

    Cleaned up, simplified, added more game logic.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
@@ -8,7 +9,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.togglebutton import ToggleButton
-from kivy.properties import StringProperty, ObjectProperty,NumericProperty,ListProperty
+from kivy.properties import StringProperty, ObjectProperty,NumericProperty,ListProperty,BooleanProperty
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from functools import partial
@@ -64,29 +65,41 @@ cards = [
 
 class MatchingGame(Widget):
     grid = random.sample(cards, len(cards))
-    characters = range (1, len(grid) + 1)
+    characters = range (0, len(grid))
     matched = []
     selected = []
+    text_color = ListProperty([1, 1, 1])
+
     
 # Function to pass back info about the clicks
-    def passed_back(self, arg):
-        clickedOn = MatchingGame.grid[arg - 1]
+    def passed_back(self, arg, button):
+        clickedOn = MatchingGame.grid[arg]
+        clickedOn.button = button
         if clickedOn not in MatchingGame.matched:
             print "clicked on = %s" % clickedOn.name
-        if MatchingGame.selected and MatchingGame.selected[0].kind == clickedOn.kind:   
+        MatchingGame.selected.append(clickedOn)
+        if len(MatchingGame.selected) != 2:
+            print "Selected:"
+            print MatchingGame.selected[0].kind  
+            return
+        if MatchingGame.selected[0].kind == MatchingGame.selected[1].kind:   
             print "MATCH!"
-            MatchingGame.selected.append(clickedOn)
             MatchingGame.matched.append(MatchingGame.selected)
-            MatchingGame.selected = []
             print "Matched:" 
             print [(fst.name, snd.name) for (fst, snd) in MatchingGame.matched]
-        elif MatchingGame.selected and MatchingGame.selected[0].kind != clickedOn.kind:
-            print "NO MATCH!"
+            for i in MatchingGame.selected:  
+                i.button.color = [1, 0, 0, 1]
+                print "yay! %s, %s!!" % (fst.name, snd.name)               
             MatchingGame.selected = []
-        else:
-            MatchingGame.selected.append(clickedOn) 
-            print "Selected:"
-            print MatchingGame.selected[0].kind
+        elif MatchingGame.selected[0].kind != MatchingGame.selected[1].kind:   
+            print "NO MATCH!"
+            for i in MatchingGame.selected:
+                pass
+                #time.sleep(2)
+                #i.button.state = 'normal'
+            MatchingGame.selected = []
+ 
+
 
 class MatchingApp(App):
     def build(self):
